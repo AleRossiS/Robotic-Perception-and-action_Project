2133df7c8ae54cbe2f691467966ea337aabc5ed2
AleRossiS <129047440+AleRossiS@users.noreply.github.com>
2025-12-11 11:59:38 +0100

    Modificeh Claudia per trovare dati imu

--- Matlab/estraz_dati_sens_fus.m
+++ Matlab/estraz_dati_sens_fus.m
@@ -1,5 +1,7 @@
 %% --- 1. Caricamento Dati ---
 
+addpath("data");
+
 % Specifica il nome dei tuoi file
 fileNameARUCO = 'SensorFusion.H_initial_walker_aruco.csv';
 fileNameENC   = 'SensorFusion.encoders.csv';
@@ -46,7 +48,7 @@ Tempo_Synched = T_ENC_UNIQUE;
 encoder_left_V = AggregatedData_ENC(:, 1);
 encoder_right_V = AggregatedData_ENC(:, 2);
 
-disp('Sincronizzazione basata sul tempo degli Encoders (dati puliti).');
+%disp('Sincronizzazione basata sul tempo degli Encoders (dati puliti).');
 
 
 % --- Pulizia ARUCO ---
@@ -120,18 +122,18 @@ DegZ_ARUCO = interp1(T_ARUCO_V, DegZ_ARUCO_V, Tempo_Synched, 'pchip');
 
 % --- 3.2. Interpolazione Dati IMU ---
 
-OmegaX_IMU = interp1(T_IMU_V, OmegaX_IMU_V, Tempo_Synched, 'pchip');
-OmegaY_IMU = interp1(T_IMU_V, OmegaY_IMU_V, Tempo_Synched, 'pchip');
-OmegaZ_IMU = interp1(T_IMU_V, OmegaZ_IMU_V, Tempo_Synched, 'pchip');
+OmegaX_IMU = interp1(T_IMU_V, OmegaX_IMU_V, Tempo_Synched, 'pchip') * RadToDeg;
+OmegaY_IMU = interp1(T_IMU_V, OmegaY_IMU_V, Tempo_Synched, 'pchip') * RadToDeg;
+OmegaZ_IMU = interp1(T_IMU_V, OmegaZ_IMU_V, Tempo_Synched, 'pchip') * RadToDeg;
 
 AccX_IMU = interp1(T_IMU_V, AccX_IMU_V, Tempo_Synched, 'pchip');
 AccY_IMU = interp1(T_IMU_V, AccY_IMU_V, Tempo_Synched, 'pchip');
 AccZ_IMU = interp1(T_IMU_V, AccZ_IMU_V, Tempo_Synched, 'pchip');
 
 
-% --- 3.3. Dati Encoders (Già Allineati e Puliti) ---
-encoder_left = encoder_left_V;
-encoder_right = encoder_right_V;
+% --- 3.3  Dati Encoders (Già Allineati e Puliti) ---
+Left_ENC = encoder_left_V;
+Right_ENC = encoder_right_V;
 
 
 %% --- 4. Creazione Tabella Sincronizzata (T_synced) ---
@@ -158,16 +160,145 @@ T_synced.AccY_IMU = AccY_IMU;
 T_synced.AccZ_IMU = AccZ_IMU;
 
 % Encoders
-T_synced.encoder_left = encoder_left;
-T_synced.encoder_right = encoder_right;
+T_synced.encoder_left = Left_ENC;
+T_synced.encoder_right = Right_ENC;
 
 
 % Rinominiamo le colonne in modo leggibile e specifico
 T_synced.Properties.VariableNames = {'Tempo [s]', ...
     'X_ARUCO [m]', 'Y_ARUCO [m]', 'Z_ARUCO [m]', ...
     'DegX_ARUCO [deg]', 'DegY_ARUCO [deg]', 'DegZ_ARUCO [deg]', ...
-    'OmegaX_IMU [rad/s]', 'OmegaY_IMU [rad/s]', 'OmegaZ_IMU [rad/s]', ...
+    'OmegaX_IMU [deg/s]', 'OmegaY_IMU [deg/s]', 'OmegaZ_IMU [deg/s]', ...
     'AccX_IMU [g]', 'AccY_IMU [g]', 'AccZ_IMU [g]', ...
     'Left_ENC [tic]', 'Right_ENC [tic]'};
 
+%% --- Matrice di Rototraslazione con Formalismo 4x4 (Skew + Translation) ---
+% Questo script costruisce le matrici W e H in formato 4x4 (se(3)) per ogni
+% istante temporale e applica la trasformazione di similarità T * M * inv(T).
+
+if ~exist('T_synced', 'var')
+    error('Errore: La tabella T_synced non è disponibile.');
+end
+
+%% 1. Costruzione della Matrice di Trasformazione T (Costante)
+
+% Rotazioni
+alpha = pi/2; % Z (Yaw)
+beta = pi;    % X (Roll)
+
+Rz = [cos(alpha), -sin(alpha), 0; sin(alpha),  cos(alpha), 0; 0, 0, 1];  
+Rx = [1,  0, 0; 0,  cos(beta), -sin(beta); 0, sin(beta),  cos(beta)];
+R = Rx * Rz; 
+
+% Traslazione (cm -> m)
+Tx_m = 70 / 100; 
+Ty_m = 20 / 100; 
+Tz_m = 0;
+p = [Tx_m; Ty_m; Tz_m];
+
+% Matrice T (4x4)
+T = [R, p; 0, 0, 0, 1];
+
+% Calcoliamo l'inversa di T una volta sola (per efficienza)
+T_inv = inv(T);
+
+% disp('Matrice T costruita:');
+% disp(T);
+
+
+%% 2. Loop di Trasformazione sui Dati IMU
+
+% Estrai i dati grezzi
+Om_x = table2array(T_synced(:, "OmegaX_IMU [deg/s]"));
+Om_y = table2array(T_synced(:, "OmegaY_IMU [deg/s]"));
+Om_z = table2array(T_synced(:, "OmegaZ_IMU [deg/s]"));
+
+Acc_x = table2array(T_synced(:, "AccX_IMU [g]"));
+Acc_y = table2array(T_synced(:, "AccY_IMU [g]"));
+Acc_z = table2array(T_synced(:, "AccZ_IMU [g]"));
+
+N = length(Om_x);
+
+% Preallocazione risultati
+Om_Fixed = zeros(N, 3);
+Acc_Fixed = zeros(N, 3);
+
+% disp('Inizio elaborazione matriciale W e H per ogni step...');
+
+for k = 1:N
+    % --- 1. Costruzione Matrice W (Velocità Angolare) ---
+    % W contiene la Skew Matrix di Omega nella parte rotazionale (3x3)
+    % e zeri nella parte traslazionale (assumiamo vel lineare non nota o separata)
+    
+    w_vec = [Om_x(k); Om_y(k); Om_z(k)];
+    
+    % Matrice Skew-Symmetric di omega
+    Omega_Skew = [0,       -w_vec(3),  w_vec(2);
+                  w_vec(3), 0,        -w_vec(1);
+                 -w_vec(2), w_vec(1),  0       ];
+             
+    % Assemblaggio W (4x4)
+    W_imu = [Omega_Skew, [0;0;0]; 
+             0, 0, 0,     0     ];
+         
+    % --- 2. Costruzione Matrice H (Accelerazione Lineare) ---
+    % H contiene l'accelerazione nella parte Traslazionale (colonna 4)
+    % e zeri nella parte rotazionale (assumiamo acc angolare non nota)
+    
+    a_vec = [Acc_x(k); Acc_y(k); Acc_z(k)];
+    
+    % Assemblaggio H (4x4)
+    H_imu = [zeros(3,3), a_vec;
+             0, 0, 0,    0    ];
+         
+    % --- 3. Applicazione Change of RF (Similarity Transform) ---
+    % Formula: M_fixed = T * M_imu * inv(T)
+    
+    W_fixed_mat = T * W_imu * T_inv;
+    H_fixed_mat = T * H_imu * T_inv;
+    
+    % --- 4. Estrazione dei Vettori Trasformati ---
+    
+    % Estrai Omega dal skew block di W_fixed_mat (o con unskew)
+    % W_fixed_mat(3,2) è omega_x, W_fixed_mat(1,3) è omega_y, etc.
+    % Usiamo la funzione unskew manuale per sicurezza:
+    omega_new = [W_fixed_mat(3,2); W_fixed_mat(1,3); W_fixed_mat(2,1)];
+    
+    % Estrai Accel dalla parte traslazionale di H_fixed_mat (prime 3 righe, col 4)
+    acc_new = H_fixed_mat(1:3, 4);
+    
+    % Salva nei vettori risultato
+    Om_Fixed(k, :) = omega_new';
+    Acc_Fixed(k, :) = acc_new';
+end
+
+%% 3. Aggiornamento Tabella
+
+% Rimuovi vecchie colonne
+T_synced(:, {'OmegaX_IMU [deg/s]', 'OmegaY_IMU [deg/s]', 'OmegaZ_IMU [deg/s]', ...
+            'AccX_IMU [g]', 'AccY_IMU [g]', 'AccZ_IMU [g]'}) = [];
+
+% Inserisci nuove colonne
+T_synced.OmegaX_Fixed = Om_Fixed(:, 1);
+T_synced.OmegaY_Fixed = Om_Fixed(:, 2);
+T_synced.OmegaZ_Fixed = Om_Fixed(:, 3);
+T_synced.AccX_Fixed = Acc_Fixed(:, 1);
+T_synced.AccY_Fixed = Acc_Fixed(:, 2);
+T_synced.AccZ_Fixed = Acc_Fixed(:, 3);
+
+% Rinomina
+T_synced.Properties.VariableNames{'OmegaX_Fixed'} = 'OmegaX_IMU_Fixed [deg/s]';
+T_synced.Properties.VariableNames{'OmegaY_Fixed'} = 'OmegaY_IMU_Fixed [deg/s]';
+T_synced.Properties.VariableNames{'OmegaZ_Fixed'} = 'OmegaZ_IMU_Fixed [deg/s]';
+T_synced.Properties.VariableNames{'AccX_Fixed'} = 'AccX_IMU_Fixed [g]';
+T_synced.Properties.VariableNames{'AccY_Fixed'} = 'AccY_IMU_Fixed [g]';
+T_synced.Properties.VariableNames{'AccZ_Fixed'} = 'AccZ_IMU_Fixed [g]';
+
+% disp('Trasformazione completata usando matrici 4x4 (Skew/Trans) e formula T*M*inv(T).');
+
 T_synced
+% %% --- 5. Salvataggio della Tabella Sincronizzata ---
+% 
+% writetable(T_synced, 'T_synched_dati_finali_puliti.csv');
+% 
+% disp('Estrazione e sincronizzazione completate. T_synched_dati_finali_puliti.csv è stato salvato.');
